a62b829cc2f65ac85d50ad43d82b31a2a600d815
Tyler Wymer <twymer@gmail.com>
2014-05-05 11:48:16 +0200

    Test directory cleanup

--- sonowatcher/tests/watcher_tests.py
+++ sonowatcher/tests/watcher_tests.py
@@ -6,6 +6,10 @@ import os
 class WatcherTests(TestCase):
     """
     Tests to ensure that sonosite file watching works
+
+    Most functionality was moved to server side.
+
+    `./setup.py test` from repository root
     """
 
     def setUp(self):
@@ -26,28 +30,3 @@ class WatcherTests(TestCase):
             print "media!"
             print list(watcher.all_media())
             self.assertIsNotNone(watcher.patient_exam_xml())
-
-    def testGenerateSubmission(self):
-        pass
-
-    def testGetIDs(self):
-        study_ids = [
-            "1.2.840.114340.03.000008251017183037.1.20130425.163744.0000053",
-            "1.2.840.114340.03.000008251017183037.2.20130821.094421.0000080"
-        ]
-
-        case_ids = ["JHUYIIYIUIY", "(_No_ID_)"]
-        currpath = os.path.dirname(os.path.abspath(__file__))
-        cpath = os.path.join(currpath, 'complete')
-        completes = os.listdir(cpath)
-        for c in completes:
-            watcher = SonoSiteWatcher(os.path.join(currpath, 'complete', c))
-            self.assertTrue(watcher.is_complete())
-            study_id = watcher.get_study_id()
-            case_id = watcher.get_case_id()
-
-            self.assertTrue(study_id in study_ids)
-            study_ids.remove(study_id)
-
-            self.assertTrue(case_id in case_ids)
-            case_ids.remove(case_id)
